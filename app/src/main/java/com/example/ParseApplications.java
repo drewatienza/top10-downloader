e6cce6057938e9efe9af232159544a1a4e14c09a
drewatienza <atienza.drew@gmail.com>
2019-03-26 04:56:16 -0700

    ListView and ArrayAdapter

--- app/src/main/java/com/example/ParseApplications.java
+++ app/src/main/java/com/example/ParseApplications.java
@@ -72,6 +72,11 @@ public class ParseApplications {
                 }
                 eventType = xpp.next();
             }
+            for (FeedEntry app: applications) {
+                Log.d(TAG, "***************");
+                Log.d(TAG, app.toString());
+            }
+
         } catch (Exception e) {
             status = false;
             e.printStackTrace();
